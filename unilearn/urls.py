8ef49a40fb1c8d4329e34777e7eeb04821a656fa
Melevir <melevir@gmail.com>
2013-01-29 00:04:31 +0400

    Added bootstrap

--- unilearn/urls.py
+++ unilearn/urls.py
@@ -1,12 +1,15 @@
 from django.conf.urls import patterns, include, url
-
 from django.contrib import admin
+
+from unilearn.views import IndexPageView
 
 
 admin.autodiscover()
 
 urlpatterns = patterns('',
     url(r'', include('lecture.urls')),
+
+    url(r'^$', IndexPageView.as_view(), name='index'),
 
     url(r'^admin/', include(admin.site.urls)),
 )
